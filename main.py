44a580c74cf9f1dd4ccca55ade654fe8bbc53655
Ashutosh Mohanty <ashutoshmohanty1799@mal.com>
2022-11-10 17:33:01 +0530

    Data Validation code added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
-
+from sensor.logger import logging
 from sensor.pipeline.training_pipeline import TrainPipeline
 
 if __name__ == '__main__':
-    training_pipeline = TrainPipeline()
-    training_pipeline.run_pipeline()
+    try:
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
